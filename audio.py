80fffb0c37476b27c00cc567c2f0d8d36a03a2c4
russoagustin <pickrusso.agustin@gmail.com>
2025-04-05 15:05:52 -0300

    Ahora el archivo audio.py agrega la frecuencia de muestreo en la primera linea del txt para que leer_vocales.py lo lea directamente

--- audio.py
+++ audio.py
@@ -2,10 +2,10 @@
 from pydub import AudioSegment
 import numpy as np
 
-frecuencia_muestreo = 9000
+frecuencia_muestreo = 5000
 periodo_muestreo = 1/frecuencia_muestreo
 
-audio = AudioSegment.from_file("audios/a.mp4",format="mp4")
+audio = AudioSegment.from_file("audios/b_agudo.mp4",format="mp4")
 audio = audio.set_frame_rate(frecuencia_muestreo)       #Establece la frec. de muestreo
 samples = np.array(audio.get_array_of_samples())        #Guardo las muestras en un array
 
@@ -14,6 +14,7 @@ time = np.linspace(0,len(samples)*periodo_muestreo,len(samples))    #"eje x" del
 
 diccionario = dict(zip(time,samples))
 
-with open("tarea1/muestras/output.txt", "w") as f:
+with open("muestras/output.txt", "w") as f:
+    f.write(f"# Frecuencia de muestreo: {frecuencia_muestreo}\n")
     for key, value in diccionario.items():
         f.write(f"{key}\t{value}\n")
--- leer_vocales.py
+++ leer_vocales.py
@@ -4,9 +4,28 @@ import matplotlib.pyplot as plt
 file = r"muestras/output.txt"
 
 data = np.loadtxt(file,comments='#', delimiter='\t')
-
 t = data[:,0]
 audio_signal = data[:,1]
+
+lines = None
+
+with open(file,'r') as file:
+    lines = file.readlines()
+
+fs_line = None
+for line in lines:
+    if'Frecuencia de muestreo' in line:
+        fs_line = line
+
+fs = None
+if fs_line is not None:
+    fs_value = fs_line.split(":")[1].strip()
+    fs=int(fs_value)
+else:
+    raise ValueError("No se encontró la fruecuencia de muestreo")
+
+
+
 
 #Graficar forma de onda del audio
 plt.figure(figsize=(10,4))
@@ -20,7 +39,7 @@ plt.show()
 
 #FFT
 n = len(audio_signal)
-frecuencia = np.fft.fftfreq(n,d=1/9000)
+frecuencia = np.fft.fftfreq(n,d=1/fs)
 fft_signal = np.fft.fft(audio_signal)
 
 positive_freqs = frecuencia[:n // 2]
@@ -33,5 +52,5 @@ plt.ylabel('Magnitud')
 plt.title('Espectro de Frecuencia')
 plt.grid(True)
 # Limitar el rango de frecuencias a mostrar, por ejemplo de 0 a 500 Hz
-plt.xlim(-10, 3000)
+plt.xlim(-10, 1500)
 plt.show()
